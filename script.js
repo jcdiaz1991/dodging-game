9b4ed5c6d139b4c3eb5514c7dc57ce07cd21b8f2
Juan <juandiaz9114@gmail.com>
2018-10-19 16:31:38 -0700

    Added Game Description

--- script.js
+++ script.js
@@ -154,6 +154,8 @@ function start() {
   window.addEventListener('keydown', moveDodger)
 
   START.style.display = 'none'
+  var gameDes =  document.getElementById('des');
+  gameDes.style.display = 'none'
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
