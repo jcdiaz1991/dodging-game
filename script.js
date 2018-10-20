90f8cc828442b8f8e7318d0895fa2440d69de8c5
Juan <juandiaz9114@gmail.com>
2018-10-19 17:27:05 -0700

    onload function modification

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ var gameInterval = null
  * but all of your work should happen below.
  */
 window.onload = function dodgerStartPosition(){
-  return DODGER.style.left = `${(GAME_WIDTH/2)-20}px`, DODGER.style.display="";
+  DODGER.style.left = `${(GAME_WIDTH/2)-20}px`;
+  DODGER.style.display="";
 }
 
  function checkCollision(rock) {
